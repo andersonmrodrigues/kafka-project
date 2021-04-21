b88cd826f8449b7c0ed59551b9a3120e9030c841
Anderson Rodrigues <maikbru@gmail.com>
2021-04-21 19:00:14 -0300

    done

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+g
+7src/main/java/br/com/desenv/ecommerce/EmailService.java,9\e\9e77f8a7c6a3082695ef31403f293451ac36639f
+e
+5src/main/java/br/com/desenv/ecommerce/LogService.java,e\d\edfcfbf1ddb16da67ab09b5520cda1d5b97b89ea
+g
+7src/main/java/br/com/desenv/ecommerce/NewOrderMain.java,9\b\9bd37b9479f3804684b397e615369c611fa1b2ea
+8
+commands,3\7\37929e0a3b9a27eb82473ae04ede11fbc9f9b00a
+o
+?src/main/java/br/com/desenv/ecommerce/FraudDetectorService.java,4\5\4537ae28b703df576786564455930272428b8dba
---
+++ commonkafka/src/main/java/br/com/desenv/ecommerce/KafkaService.java
@@ -0,0 +1,63 @@
+package br.com.desenv.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.Closeable;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+class KafkaService<T> implements Closeable {
+    private final KafkaConsumer<String, T> consumer;
+    private final ConsumerFunction parse;
+
+    KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
+        this(parse, groupId, type, properties);
+        consumer.subscribe(Collections.singletonList(topic));
+
+    }
+
+    KafkaService(String groupId, Pattern topic, ConsumerFunction parse, Class<T> type, Map<String, String> properties) {
+        this(parse, groupId, type, properties);
+        consumer.subscribe(topic);
+    }
+
+    private KafkaService(ConsumerFunction parse, String groupId, Class<T> type, Map<String, String> properties) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<>(getProperties(groupId, type, properties));
+    }
+
+    private Properties getProperties(String groupId, Class<T> type, Map<String, String> overrideProperties) {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDesarializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+        properties.setProperty(GsonDesarializer.TYPE_CONFIG, type.getName());
+        properties.putAll(overrideProperties);
+        return properties;
+    }
+
+    void run() {
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println("Found " + records.count() + " registers");
+                for (var record : records) {
+                    parse.consume(record);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void close() {
+        consumer.close();
+    }
+}
